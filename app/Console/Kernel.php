7f5ec75002b84d23bbbc22db798911f0857d1ee9
friederkahl <frieder.kahl@k-tronik.digital>
2021-02-11 20:59:05 +0100

    fix docker setup cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('inspire')->hourly();
          $schedule->command('api:update-maps')->everyMinute();
          $schedule->command('api:update-dynamic-data')->everyMinute();
     }
